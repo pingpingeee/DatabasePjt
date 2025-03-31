7635e95a53d4a466be2be792a8007b40c8e62567
manamjong <112713623+pingpingeee@users.noreply.github.com>
2025-03-31 22:47:01 +0900

    정리

--- src/control/Join_LoginVO.java
+++ src/control/Join_LoginVO.java
@@ -3,56 +3,56 @@ package control;
 import java.sql.Date;
 
 public class Join_LoginVO {
-    String id;
-    String pw;
-    String name;
-    String phnum;
-    int type;
-    String speciality;
+    // String id;
+    // String pw;
+    // String name;
+    // String phnum;
+    // int type;
+    // String speciality;
 
-    public String getSpeciality() {
-		return speciality;
-	}
+    // public String getSpeciality() {
+	// 	return speciality;
+	// }
 
-	public void setSpeciality(String speciality) {
-		this.speciality = speciality;
-	}
+	// public void setSpeciality(String speciality) {
+	// 	this.speciality = speciality;
+	// }
 
-	public String getId() {
-        return id;
-    }
+	// public String getId() {
+    //     return id;
+    // }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    // public void setId(String id) {
+    //     this.id = id;
+    // }
 
-    public String getPw() {
-        return pw;
-    }
+    // public String getPw() {
+    //     return pw;
+    // }
 
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-    public String getName() {
-        return name;
-    }
+    // public void setPw(String pw) {
+    //     this.pw = pw;
+    // }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPhnum() {
-        return phnum;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+    // public String getPhnum() {
+    //     return phnum;
+    // }
 
-    public void setPhnum(String phnum) {
-        this.phnum = phnum;
-    }
-    public int getType() {
-        return type;
-    }
+    // public void setPhnum(String phnum) {
+    //     this.phnum = phnum;
+    // }
+    // public int getType() {
+    //     return type;
+    // }
 
-    public void setType(int type) {
-        this.type = type;
-    }
+    // public void setType(int type) {
+    //     this.type = type;
+    // }
     
 }
